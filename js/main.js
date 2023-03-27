93d30cc7cbe28f1bfb3ca8b4f3cc99600fa407ee
Brian <bstanton773@gmail.com>
2023-03-27 14:13:18 -0500

    Primitive + Object Data types

--- js/main.js
+++ js/main.js
@@ -47,3 +47,92 @@ console.log(typeof myAge);
 var pi = 3.14;
 console.log(pi);
 console.log(typeof pi);
+
+
+// Boolean
+var trueBool = true;
+console.log(trueBool);
+console.log(typeof trueBool);
+
+var boolFalse = false;
+console.log(boolFalse);
+console.log(typeof boolFalse);
+
+
+// Undefined
+var something;
+console.log(something);
+console.log(typeof something);
+
+// Null
+var someNull = null;
+console.log(someNull);
+console.log(typeof someNull); // A Bug in the code!
+
+var myCity;
+
+console.log(myCity);
+console.log(typeof myCity);
+
+myCity = 'Chicago';
+console.log(myCity);
+console.log(typeof myCity);
+
+
+// Object Data Types
+
+// Object
+var myObject = {};
+
+console.log(myObject);
+console.log(typeof myObject);
+
+var anotherObject = {
+    prop1: 'Hello World',
+    abc: 123
+};
+
+console.log(anotherObject);
+console.log(typeof anotherObject);
+
+// Array
+var firstArray = [];
+console.log(firstArray);
+console.log(typeof firstArray);
+
+var newArray = ['Brian', 'Michael', 'Danielle', 'Frank']
+console.log(newArray);
+console.log(typeof newArray);
+
+// Function
+function abc(){};
+
+console.log(abc);
+console.log(typeof abc);
+
+
+// Accessing properties from an object
+var person = {
+    first: 'Emily',
+    last: 'Murphy',
+    languages: ['JavaScript', 'Python']
+};
+
+console.log(person);
+
+// Bracket Notation
+console.log(person['first']);
+// Dot Notation
+console.log(person.last);
+
+console.log(person.languages);
+
+// When accessing properties in an array (aka the indices), use bracket notation
+console.log(person.languages[0])
+console.log(person.languages[1])
+
+// All Array objects also have a "length" property
+console.log(person.languages.length);
+
+// Clear the console
+console.clear();
